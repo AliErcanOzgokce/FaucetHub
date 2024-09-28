433af05733d0b22be3c0c3dc5d77643cffb04305
AliErcanOzgokce <aliercanozgokce@gmail.com>
2024-09-28 17:12:44 +0300

    Navbar updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,3 @@
-// components/Navbar.tsx
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
@@ -11,27 +10,9 @@ function Navbar() {
         <Link href="/">
           <Image src={logo} alt="logo" width={150} height={50} className="cursor-pointer" />
         </Link>
-        <nav className="hidden md:flex space-x-8">
-          <Link href="/home" className="relative text-gray-700 hover:text-gray-900 transition duration-300">
-            <span>Home</span>
-            <span className="absolute bottom-[-4px] left-0 w-0 h-0.5 bg-blue-600 transition-all duration-300"></span>
-          </Link>
-          <Link href="/features" className="relative text-gray-700 hover:text-gray-900 transition duration-300">
-            <span>Features</span>
-            <span className="absolute bottom-[-4px] left-0 w-0 h-0.5 bg-blue-600 transition-all duration-300"></span>
-          </Link>
-          <Link href="/pricing" className="relative text-gray-700 hover:text-gray-900 transition duration-300">
-            <span>Pricing</span>
-            <span className="absolute bottom-[-4px] left-0 w-0 h-0.5 bg-blue-600 transition-all duration-300"></span>
-          </Link>
-          <Link href="/contact" className="relative text-gray-700 hover:text-gray-900 transition duration-300">
-            <span>Contact</span>
-            <span className="absolute bottom-[-4px] left-0 w-0 h-0.5 bg-blue-600 transition-all duration-300"></span>
-          </Link>
-        </nav>
         <button
           data-theme="myTheme"
-          className="hidden md:inline-block px-6 py-2 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-300"
+          className="px-6 py-2 bg-gray-800 text-white font-semibold rounded-full hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-800 focus:ring-opacity-50 transition duration-300"
         >
           Support Us!
         </button>
