efe01ff3266717483459d4eab11ad5b396147afa
AliErcanOzgokce <aliercanozgokce@gmail.com>
2024-10-18 21:35:01 +0300

    Adding analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
@@ -17,6 +18,7 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Footer />
+          <Analytics />
         </main>
       </body>
     </html>
